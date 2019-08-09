1a39cfbeb848a3b7e6e6307b253a96549ce92d99
Elton SV <el10savio96@gmail.com>
2019-08-09 19:35:44 +0530

    Header & AddBar Basic CSS

--- frontend/src/components/addbar.js
+++ frontend/src/components/addbar.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./styles/Addbar.css";
 
 function AddBar() {
-  return <div className="AddBar">AddBar</div>;
+  return (
+    <div className="AddBar">
+      <div className="AddBar-Text">AddBar Text</div>
+      <div className="AddBar-Button">AddBar Button</div>
+    </div>
+  );
 }
 
 export default AddBar;
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/Header.css";
 
 function Header() {
-  return <div className="Header"> Header </div>;
+  return <div className="Header"> TODO List </div>;
 }
 
 export default Header;
