55ebc8cdfbd8d431109e8ab9da64b949e831adce
Elton SV <el10savio96@gmail.com>
2019-08-10 08:41:12 +0530

    Updated TodoList CSS

--- frontend/src/components/todolist.js
+++ frontend/src/components/todolist.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./styles/Todolist.css";
 
 function Todolist() {
-  return <div className="TodoList">Todolist</div>;
+  return (
+    <div className="TodoList">
+      <div className="List">
+        <div className="ListItem">
+          <div className="Title">Title</div>
+          <div className="Status">Status</div>
+        </div>
+        <div className="ListItem">
+          <div className="Title">Title</div>
+          <div className="Status">Status</div>
+        </div>
+        <div className="ListItem">
+          <div className="Title">Title</div>
+          <div className="Status">Status</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Todolist;
