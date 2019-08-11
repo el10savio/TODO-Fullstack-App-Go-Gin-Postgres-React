f9a2ecead49fc470a5945937680fe0aa12f167e1
Elton SV <el10savio96@gmail.com>
2019-08-11 19:02:05 +0530

    TODO List Adds Items

--- frontend/src/components/addbar.js
+++ frontend/src/components/addbar.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./styles/Addbar.css";
 
 class AddBar extends React.Component {
+  addItem = event => {
+    if (event.key === "Enter") {
+      fetch(`http://localhost:8081/item/create/${event.target.value}`).then(
+        alert("Bug: Reload The Page To View Changes")
+      );
+    }
+  };
+
   render() {
     return (
       <div className="AddBar">
@@ -9,8 +17,8 @@ class AddBar extends React.Component {
           className="AddBar-Text"
           type="text"
           placeholder="Enter TODO Item"
+          onKeyDown={this.addItem}
         />
-        <div className="AddBar-Button">Add Item</div>
       </div>
     );
   }
